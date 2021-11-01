4a3dad9248e45c1a92dc59179cfdfc3358990be7
Lyrrad96 <darryl.r.andrade@gmail.com>
2021-11-01 11:21:47 +0530

    Figured out how to connect form to database

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
+import axios from 'axios'
+
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Login.scss";
-const mongoose = require('mongoose')
+
+/*const mongoose = require('mongoose')
 const signUpTemplate = new mongoose.Schema({
   fullname: {
     type:String,
@@ -22,44 +25,58 @@ const signUpTemplate = new mongoose.Schema({
   }
 })
 module.exports = mongoose.model('mytable', signUpTemplate)
+*/
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
 
-export default function Login() {
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
+    this.state = {
+      username: '',
+    }
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  onChangeUsername(e) {
+    this.setState({
+      username:e.target.value
+    })
   }
 
-  return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-      </Form>
+  onSubmit(e) {
+    e.preventDefault()
+
+    const user = {
+      username:this.state.username, 
+    }
+
+    console.log(user)
+
+    axios.post('http://localhost:5000/users/add', user)
+    .then(res => console.log(res.data));
+
+
+    this.setState({
+      username: '',
+    })
+      
+    
+  }
+render(){
+  return(
+    <div className="Login ">
+      <h3>Create User</h3>
+      <form onSubmit={this.onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <input type="text" required className="form-control" defaultValue={this.state.username} onChange={this.onChangeUsername}/>
+        </div>
+        <div className="form-group">
+          <input type="submit" value="Create User" className="btn btn-primary"/>
+        </div>
+      </form>
     </div>
-  );
+  )
+}
 }
